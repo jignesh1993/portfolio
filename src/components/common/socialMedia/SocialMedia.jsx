52e680d2135c80a7a8f782afcd0e3a172acdb46b
jignesh patel <jignesh.mahesh07@gmail.com>
2025-12-05 21:18:00 +0530

    update content

--- src/components/common/socialMedia/SocialMedia.jsx
+++ src/components/common/socialMedia/SocialMedia.jsx
@@ -4,21 +4,28 @@ import {
   faFacebookF,
   faInstagram,
   faLinkedin,
+  faGithub,
+  faStackOverflow,
+  faUpwork
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const socialIcons = [
-  { icon: faFacebookF, link: "#!" },
-  { icon: faDribbble, link: "#!" },
-  { icon: faInstagram, link: "#!" },
-  { icon: faLinkedin, link: "#!" },
-  { icon: faBehance, link: "#!" },
+  // { icon: faDribbble, link: "#!" },
+  { icon: faLinkedin, link: "https://www.linkedin.com/in/jignesh-patel-007/" },
+  { icon: faGithub, link: "https://github.com/jignesh1993" },
+  { icon: faStackOverflow, link: "https://stackoverflow.com/users/13397319/jignesh-patel" },
+  { icon: faUpwork, link: "https://www.upwork.com/freelancers/~0121fcedc9cefe1af7" },
+  { icon: faInstagram, link: "https://www.instagram.com/jignesh_mahesh_patel_25?igsh=MTZjc2xlMHUxZ2U3aQ==" }
+  // { icon: faBehance, link: "#!" }
 ];
 
 const SocialMedia = () => {
   return socialIcons.map((item, index) => (
     <a
       href={item.link}
+      target="_blank"
+      rel="noopener noreferrer"
       className={`text-picto-primary hover:bg-picto-primary p-2 pt-3 xs:p-2.5 xs:pt-3.75 sm:pt-4 md:pt-5 sm:p-3 md:p-3.75 hover:text-white rounded-md`}
       key={index}
     >
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -11,17 +11,17 @@ const addressData = [
   {
     icon: faLocationDot,
     title: "Address",
-    description: "New Mexico, 31134",
+    description: "Navsari, Gujarat, India 396445",
   },
   {
     icon: faEnvelope,
     title: "My Email",
-    description: "mymail@mail.com",
+    description: "jignesh.mahesh07@gmail.com",
   },
   {
     icon: faPhone,
     title: "Call Me Now",
-    description: "00-123 00000",
+    description: "+91 9099377527",
   },
 ];
 
